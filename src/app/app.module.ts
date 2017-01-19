7c996e0ed59d4e9b2ee60a99574765fcd6e99c66
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2017-01-19 15:42:40 +0100

    Added a search component Currently it only shows a list of some hardcoded employee names.
    * Added md2 dependency to package.json and imports
    * Added SearchComponent to declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {SearchComponent} from "./search/search.component";
+import {Md2Module} from "md2";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Md2Module.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
