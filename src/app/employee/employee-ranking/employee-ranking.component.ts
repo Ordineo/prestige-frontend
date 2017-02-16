a57760fda9e39863235137ac7d9e5df1eddd4f61
Ines Van Stappen <ines.vanstappen@gmail.com>
2017-02-16 14:28:27 +0100

    cards added

--- src/app/employee/employee-ranking/employee-ranking.component.ts
+++ src/app/employee/employee-ranking/employee-ranking.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {EmployeeService} from "../../providers/employee.service";
-import {Observable} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from "../../providers/employee.service";
+import { Observable } from "rxjs";
 
 import 'rxjs/add/operator/merge';
 
-import {FirebaseObjectObservable} from "angularfire2";
-import {forEach} from "@angular/router/src/utils/collection";
+import { FirebaseObjectObservable } from "angularfire2";
+import { forEach } from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-employee-ranking',
@@ -15,15 +15,22 @@ import {forEach} from "@angular/router/src/utils/collection";
 export class EmployeeRankingComponent implements OnInit {
   employees: Observable <any>;
   totalPrestige = 1;
-
+  availableColors = [
+    { name: 'none', color: '' },
+    { name: 'Primary', color: 'primary' },
+    { name: 'Accent', color: 'accent' },
+    { name: 'Warn', color: 'warn' }
+  ];
+  
   constructor(private employeeService: EmployeeService) {
+    
   }
-
+  
   ngOnInit() {
     this.employeeService.getEmployees()
       .subscribe(result => {
         this.employees = result;
       });
   }
-
+  
 }
