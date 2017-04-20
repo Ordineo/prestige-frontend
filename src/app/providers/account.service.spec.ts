aff1404c9ee1697ff61e9ea9531b73a819abe21b
Ines Van Stappen <ines.vanstappen@gmail.com>
2017-04-20 16:22:29 +0200

    removed this.

--- src/app/providers/account.service.spec.ts
+++ src/app/providers/account.service.spec.ts
@@ -33,7 +33,7 @@ describe('AccountService', () => {
       status: 200
     }));
 
-    let httpMock = HelpFunctions.getMockHttp({post: mockResponse});
+    let httpMock = getMockHttp({post: mockResponse});
     let accountService = new AccountService(httpMock);
 
     accountService.login("test", "password").subscribe((result) => {
