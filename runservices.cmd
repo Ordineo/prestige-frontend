440375854b1b7475df5700b1dea4277ce1194812
Tyranwyn <sammi.fux@hotmail.com>
2017-05-26 12:32:42 +0200

    Deleted unnecessary files

--- runservices.cmd
+++
@@ -1,4 +0,0 @@
-start /min cmd /c node src/app/shared/gatekeeper/server.js
-start /min cmd /c "%ProgramFiles%\MongoDB\Server\3.4\bin\mongod.exe" --dbpath data
-start /min cmd /c node-red
-start /min cmd /c ng serve
