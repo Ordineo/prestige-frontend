6f2918c46d6e431ae68e0bd8aeab5518b5498756
van Hartingsveldt <Ivar.vanHartingsveldt@ordina.be>
2017-06-06 09:50:42 +0100

    removed bootstrap, extracted the toolbar / navbar components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import {NgModule}              from '@angular/core';
-import {RouterModule, Routes}  from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import {AppComponent} from './app.component';
-import {EmployeeDetailComponent} from './employee/employee-detail/employee-detail.component';
-import {EmployeeRankingComponent} from './employee/employee-ranking/employee-ranking.component';
-import {PrestigeDetailComponent} from "./prestige/prestige-detail/prestige-detail.component";
-import {PrestigeFeedComponent} from './prestige/prestige-feed/prestige-feed.component';
-import {SearchComponent} from './shared/search/search.component';
-import {AuthComponent} from "./shared/auth/auth.component";
-import {LoginComponent} from "./login/login.component";
+import { AppComponent } from './app.component';
+import { EmployeeDetailComponent } from './employee/employee-detail/employee-detail.component';
+import { EmployeeRankingComponent } from './employee/employee-ranking/employee-ranking.component';
+import { PrestigeDetailComponent } from './prestige/prestige-detail/prestige-detail.component';
+import { PrestigeFeedComponent } from './prestige/prestige-feed/prestige-feed.component';
+import { SearchComponent } from './shared/search/search.component';
+import { AuthComponent } from './shared/auth/auth.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
   {
