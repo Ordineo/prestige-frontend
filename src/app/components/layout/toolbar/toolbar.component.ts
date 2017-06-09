5d45343a796cf83676103b501f84d68f593897eb
van Hartingsveldt <Ivar.vanHartingsveldt@ordina.be>
2017-06-09 06:53:14 +0100

    added more tests

--- src/app/components/layout/toolbar/toolbar.component.ts
+++ src/app/components/layout/toolbar/toolbar.component.ts
@@ -1,72 +1,71 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
-import { Router } from '@angular/router';
-import { MdDialog, MdDialogConfig, MdDialogRef } from '@angular/material';
-import { AccountDetailComponent } from '../../account/account-detail/account-detail.component';
-import { CookieService } from 'ngx-cookie';
-import { AuthService } from '../../../services/auth.service';
-import { AddEndorsementComponent } from '../../endorsements/add-endorsement/add-endorsement.component';
-import { UserService } from '../../../services/user.service';
+import {Component, OnInit} from '@angular/core';
+import {MdDialog, MdDialogConfig, MdDialogRef} from '@angular/material';
+import {AccountDetailComponent} from '../../account/account-detail/account-detail.component';
+import {CookieService} from 'ngx-cookie';
+import {AuthService} from '../../../services/auth.service';
+import {AddEndorsementComponent} from '../../endorsements/add-endorsement/add-endorsement.component';
+import {UserService} from '../../../services/user.service';
 
 @Component({
-    selector: 'app-toolbar',
-    templateUrl: 'toolbar.component.html',
-    styleUrls: ['./toolbar.component.scss']
+  selector: 'app-toolbar',
+  templateUrl: 'toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
 })
 
 export class ToolbarComponent implements OnInit {
 
-    dialogRef: MdDialogRef<any>;
-    username: string;
+  dialogRef: MdDialogRef<any>;
+  username: string;
 
-    constructor(private router: Router,
-        private dialog: MdDialog,
-        private viewContainerRef: ViewContainerRef,
-        private cookieService: CookieService,
-        private authService: AuthService,
-        private userService: UserService) { }
+  constructor(private dialog: MdDialog,
+              private cookieService: CookieService,
+              private authService: AuthService,
+              private userService: UserService) {
+  }
 
-    ngOnInit() {
-        this.username = this.cookieService.get('username');
-    }
+  ngOnInit() {
+    this.username = this.cookieService.get('username');
+  }
 
-    public isUserLoggedIn(): boolean {
-        return this.userService.isUserLoggedIn();
-    }
+  public isUserLoggedIn(): boolean {
+    return this.userService.isUserLoggedIn();
+  }
 
-    public getLoggedInUsername(): string | undefined {
-        return this.userService.getCurrentUsername();
-    }
+  public getLoggedInUsername(): string | undefined {
+    return this.userService.getCurrentUsername();
+  }
 
-    public getLoggedInUserAvatar(): string | undefined {
-        return this.userService.getCurrentUser() ? this.userService.getCurrentUser().avatar : undefined;
-    }
+  public getLoggedInUserAvatar(): string | undefined {
+    return this.userService.getCurrentUser() ? this.userService.getCurrentUser().avatar : undefined;
+  }
 
-    public openAccountDetail() {
-        const config = new MdDialogConfig();
-        config.viewContainerRef = this.viewContainerRef;
-        config.width = '80%';
+  public openAccountDetail() {
+    const config = new MdDialogConfig();
+    config.width = '80%';
 
-        this.dialogRef = this.dialog.open(AccountDetailComponent, config);
+    this.dialogRef = this.dialog.open(AccountDetailComponent, config);
 
-        this.dialogRef.afterClosed().subscribe(result => {
-            this.dialogRef = null;
-        });
-    }
+    this.dialogRef
+      .afterClosed()
+      .subscribe(() => {
+        this.dialogRef = null;
+      });
+  }
 
-    public openAddPrestige() {
-        const config = new MdDialogConfig();
-        config.viewContainerRef = this.viewContainerRef;
-        config.width = '80%';
+  public openAddPrestige() {
+    const config = new MdDialogConfig();
+    config.width = '80%';
 
-        this.dialogRef = this.dialog.open(AddEndorsementComponent, config);
+    this.dialogRef = this.dialog.open(AddEndorsementComponent, config);
 
-        this.dialogRef.afterClosed().subscribe(result => {
-            this.dialogRef = null;
-            // reload endorsements
-        });
-    }
+    this.dialogRef
+      .afterClosed()
+      .subscribe(() => {
+        this.dialogRef = null;
+      });
+  }
 
-    public logout() {
-        this.authService.logout();
-    }
+  public logout() {
+    this.authService.logout();
+  }
 }
