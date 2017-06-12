6e5215fb83b5d155f71c5eee9027480b052d1265
van Hartingsveldt <Ivar.vanHartingsveldt@ordina.be>
2017-06-12 07:57:09 +0100

    added some changes to the authentication pages, removed inline styling

--- src/app/models/account.ts
+++ src/app/models/account.ts
@@ -1,10 +1,10 @@
 export class Account {
     public username: string;
+    public email: string;
     public firstName: string;
     public lastName: string;
-    public email: string;
+    public avatar: string;
     public phone: string;
     public unit: string;
     public gender: string;
-    public avatar: string;
 }
