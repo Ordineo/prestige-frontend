fff7ed6f333b4bfccd51eb48f1d278df5b64f4c5
ivarvh <IvhRvb1207gi>
2017-06-13 10:27:15 +0200

    wrote more tests for the auth service

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,14 +1,20 @@
 import {AuthService} from './auth.service';
-import {Http, Response, ResponseOptions} from '@angular/http';
+import {Response, ResponseOptions} from '@angular/http';
 import {UserService} from './user.service';
 import {Router} from '@angular/router';
 import {EmployeeService} from './employee.service';
-import {anyString, anything, instance, mock, verify, when} from 'ts-mockito';
+import {instance, mock, verify, when, deepEqual, capture} from 'ts-mockito';
 import {Subject} from 'rxjs/Subject';
 import {Account} from '../models/account';
 import {PrestigeHttp} from './prestige-http.service';
+import {environment} from '../../environments/environment';
 
 describe('AuthService', () => {
+
+  const loginEndPoint = `${environment.endPoint}/employees-service/login`;
+  const registerEndpoint = `${environment.endPoint}/employees-service/register`;
+  const username = 'username';
+  const password = 'password';
 
   let serviceUnderTest: AuthService;
   let http: PrestigeHttp;
@@ -33,17 +39,18 @@ describe('AuthService', () => {
 
     it('should do the login and save the resulting data', (done) => {
       const token = 'token';
-      const username = 'username';
       const currentAccount = new Account();
       const loginSubject = new Subject<Response>();
       const currentUserSubject = new Subject<Account>();
-      const loginObservable = loginSubject.asObservable();
-      loginObservable['test'] = 'test';
-      when(http.post(anyString(), anything(), false)).thenReturn(loginObservable);
+
+      when(http.post(deepEqual(loginEndPoint), deepEqual({
+        username,
+        password
+      }), false)).thenReturn(loginSubject.asObservable());
       when(employeeService.getByUsername(username)).thenReturn(currentUserSubject.asObservable());
 
       serviceUnderTest
-        .login(username, 'password')
+        .login(username, password)
         .subscribe(() => {
           verify(userService.saveCurrentUserToken(token)).once();
           verify(userService.saveCurrentUser(currentAccount)).once();
@@ -53,6 +60,37 @@ describe('AuthService', () => {
       loginSubject.next(createResponseWithText(token));
       currentUserSubject.next(currentAccount);
     });
+
+  });
+
+  describe('register', () => {
+
+    it('should do a request to the register url', (done) => {
+      const registerSubject = new Subject();
+
+      when(http.post(`${registerEndpoint}?username=${username}&password=${password}`, null, false))
+        .thenReturn(registerSubject.asObservable());
+
+      serviceUnderTest.register(username, password)
+        .subscribe(() => {
+          done();
+        });
+
+      registerSubject.next();
+    });
+
+  });
+
+  describe('logout', () => {
+
+    it('should call the userservice and navigate to the login', () => {
+      serviceUnderTest.logout();
+
+      verify(userService.removeCurrentUserData()).called();
+
+      const [firstArg] = capture(router.navigate).last();
+      expect(firstArg).toEqual(['/login']);
+    })
 
   });
 
