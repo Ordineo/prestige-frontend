2d3441201ce726326c420d6067b72ede8a3867c7
ivarvh <IvhRvb1207gi>
2017-06-13 13:28:27 +0200

    removed cookies and used localStorage instead

---
+++ src/app/services/local-storage.service.spec.ts
@@ -0,0 +1,89 @@
+import {LocalStorageService} from './local-storage.service';
+import {Account} from '../models/account';
+
+describe('LocalStorageService', () => {
+
+  let serviceUnderTest: LocalStorageService;
+
+  const key = 'key';
+  const value = 'value';
+
+  beforeEach(() => {
+    serviceUnderTest = new LocalStorageService();
+    spyOn(localStorage, 'setItem').and.callThrough();
+    spyOn(localStorage, 'getItem').and.callThrough();
+    spyOn(localStorage, 'removeItem').and.callThrough();
+  });
+
+  afterEach(() => {
+    localStorage.clear();
+  });
+
+  describe('setString', () => {
+
+    it('should call the localStorage api to save the value under the given key', () => {
+      serviceUnderTest.setString(key, value);
+
+      expect(localStorage.setItem).toHaveBeenCalledWith(key, value);
+    });
+
+  });
+
+  describe('getString', () => {
+
+    it('should call the localStorage api to get the value under the given key', () => {
+      serviceUnderTest.getString(key);
+
+      expect(localStorage.getItem).toHaveBeenCalledWith(key);
+    });
+
+  });
+
+  describe('setObject', () => {
+
+    it('should call the localStorage api to save the value under the given key', () => {
+      const account = new Account();
+      serviceUnderTest.setObject(key, account);
+
+      expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(account));
+    });
+
+  });
+
+  describe('getObject', () => {
+
+    it('should call the localStorage api to get the value under the given key in the correct class', () => {
+      const account = new Account();
+      serviceUnderTest.setObject(key, account)
+
+      serviceUnderTest.getObject<Account>(key);
+
+      expect(localStorage.getItem).toHaveBeenCalledWith(key);
+    });
+
+  });
+
+  describe('remove', () => {
+
+    it('should call the localStorage api to remove the value under the given key', () => {
+      serviceUnderTest.remove(key);
+
+      expect(localStorage.removeItem).toHaveBeenCalledWith(key);
+    });
+
+  });
+
+  describe('removeAll', () => {
+
+    it('should call the localStorage api to remove the values under the given keys', () => {
+      const key2 = 'key2';
+      serviceUnderTest.removeAll([key, key2]);
+
+      expect(localStorage.removeItem).toHaveBeenCalledWith(key);
+      expect(localStorage.removeItem).toHaveBeenCalledWith(key2);
+    });
+
+  });
+
+
+});
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,42 +1,41 @@
-import { Account } from '../models/account';
-import { CookieService } from 'ngx-cookie';
-import { Injectable } from '@angular/core';
-import { constants } from '../util/constants';
+import {Account} from '../models/account';
+import {CookieService} from 'ngx-cookie';
+import {Injectable} from '@angular/core';
+import {constants} from '../util/constants';
+import {LocalStorageService} from './local-storage.service';
 
 @Injectable()
 export class UserService {
 
-    constructor(
-        private cookieService: CookieService
-    ) { }
+  constructor(private localStorageService: LocalStorageService) {
+  }
 
-    getCurrentUser(): Account {
-        return this.cookieService.getObject(constants.cookieKeys.CURRENT_USER) as Account;
-    }
+  getCurrentUser(): Account {
+    return this.localStorageService.getObject<Account>(constants.storageKeys.CURRENT_USER);
+  }
 
-    getCurrentUsername(): string | undefined {
-        return this.getCurrentUser() ? this.getCurrentUser().username : undefined;
-    }
+  getCurrentUsername(): string | undefined {
+    return this.getCurrentUser() ? this.getCurrentUser().username : undefined;
+  }
 
-    isUserLoggedIn(): boolean {
-        return this.getCurrentUser() != null;
-    }
+  isUserLoggedIn(): boolean {
+    return this.getCurrentUser() != null;
+  }
 
-    getCurrentUserToken(): string | undefined {
-        return this.cookieService.get(constants.cookieKeys.TOKEN);
-    }
+  getCurrentUserToken(): string | undefined {
+    return this.localStorageService.getString(constants.storageKeys.TOKEN);
+  }
 
-    saveCurrentUserToken(token: string) {
-        this.cookieService.put(constants.cookieKeys.TOKEN, token);
-    }
+  saveCurrentUserToken(token: string) {
+    this.localStorageService.setString(constants.storageKeys.TOKEN, token);
+  }
 
-    saveCurrentUser(user: Account) {
-        this.cookieService.putObject(constants.cookieKeys.CURRENT_USER, user);
-    }
+  saveCurrentUser(user: Account) {
+    this.localStorageService.setObject(constants.storageKeys.CURRENT_USER, user);
+  }
 
-    removeCurrentUserData() {
-        this.cookieService.remove(constants.cookieKeys.CURRENT_USER);
-        this.cookieService.remove(constants.cookieKeys.TOKEN);
-    }
+  removeCurrentUserData() {
+    this.localStorageService.removeAll([constants.storageKeys.CURRENT_USER, constants.storageKeys.TOKEN]);
+  }
 
 }
