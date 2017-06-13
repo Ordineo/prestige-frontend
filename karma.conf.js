a78a9b2ce0b67afaba24871cb546efe80250702d
ivarvh <IvhRvb1207gi>
2017-06-13 08:13:02 +0200

    forgot to add coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-coverage'),
+      require('karma-coveralls'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
