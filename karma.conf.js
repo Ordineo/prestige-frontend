c12c67956219c21583b5b87783ec6436c1f00d4d
ivarvh <IvhRvb1207gi>
2017-06-13 08:13:36 +0200

    removed istanbul coverage reporter

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,6 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-phantomjs-launcher' ),
-      require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma'),
       require('karma-junit-reporter'),
       require('karma-spec-reporter'),
